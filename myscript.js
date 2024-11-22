aaeb417c0056f9f62443162cb79c997072d74fb8
nekopiiie <kayrise@bk.ru>
2024-11-22 18:45:42 +0300

    верстка полностью
    адаптива нет, анимаций нет

---
+++ myscript.js
@@ -0,0 +1,35 @@
+// var title = document.getElementById('myTitle');
+// var button =  document.getElementById('myButton');
+// var close = document.getElementById('myClose')
+// var modal = document.getElementById('myModal')
+
+
+// button.onclick = function(){
+//     modal.style.display = 'block';
+// }
+
+// close.onclick = function(){
+//     modal.style.display = 'none';
+// }
+
+// alert('господи помоги')
+// let age = prompt('сколько тебе лет?', 17);
+// let isBoss = confirm('и давно тебе 17?');
+
+// if (age > 17 & isBoss)
+// {
+//     alert('ne vresh');
+// }
+
+// if (age < 17 & isBoss)
+//     {
+//         alert('*осуждающий взгляд*');
+//     }
+
+// if (age > 17 & !isBoss){
+//         alert('мяу');
+//     }
+
+//     if (age < 17 & !isBoss){
+//         alert('знаешь ли ты вдоль ночных дорог шла босиком не жалея ног');
+//     }
